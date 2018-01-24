52ec7619b79e9dae8afdb10409424bb7fdda65e0
ulaganathanesakki <e.ulaganathan@gmail.com>
2018-01-23 22:47:39 -0800

    Alert
    Alert

---
+++ src/pages/quotes/quotes.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { NavParams, AlertController } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
+
+@Component({
+  selector: 'page-quotes',
+  templateUrl: 'quotes.html',
+})
+export class QuotesPage implements OnInit {
+  quoteGroup: { category: string, quotes: Quote[], icon: string };
+  constructor(private navParams: NavParams, private alertCtrl: AlertController ){
+}
+//Add elvis operator (?) in template to use this approach
+// ionViewDidLoad(){
+//   this.quoteGroup = this.navParams.data;
+// }
+ngOnInit(){
+  this.quoteGroup = this.navParams.data;
+}
+  onAddToFavorite(){
+    let prompt = this.alertCtrl.create({
+      title: 'Add Quote',
+      message: "Are you sure you want to add quote",
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => {
+            console.log('yes');
+          }
+        },
+        {
+          text: 'No',
+          role:'cancel',
+          handler: () => {
+            console.log('No');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+}
